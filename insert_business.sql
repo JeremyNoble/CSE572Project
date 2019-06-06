ec7a49fd60e7ecf16d87101133dac262da71d059
Jeremy Noble <005940658@coyote.csusb.edu>
2019-06-06 11:21:57 -0700

    altered a few more record details to ensure required queries will return results

--- insert_business.sql
+++ insert_business.sql
@@ -8,28 +8,28 @@ INSERT INTO BusinessClient VALUES (
 	'BC001', 'BR001', 'Real Fake Doors', '70 Sea Road', 'Detchant'
 );
 INSERT INTO BusinessClient VALUES (
-	'BC002', 'BR002', 'Planet Express', '81 Whitby Rd', 'Kirkton'
+	'BC002', 'BR002', 'Planet Express', '81 Whitby Rd', 'Dry Fork'
 );
 INSERT INTO BusinessClient VALUES (
-	'BC003', 'BR003', 'Centixo', '3 Coast Rd', 'Kilninian'
+	'BC003', 'BR003', 'Centixo', '3 Coast Rd', 'Glasgow'
 );
 INSERT INTO BusinessClient VALUES (
-	'BC004', 'BR001', 'Semiloo LLC', '81 Wood Lane', 'Bagworth'
+	'BC004', 'BR001', 'Semiloo LLC', '81 Wood Lane', 'Marshbrook'
 );
 INSERT INTO BusinessClient VALUES (
 	'BC005', 'BR002', 'Diser Company', '68 Castledore Rd', 'Detchant'
 );
 INSERT INTO BusinessClient VALUES (
-	'BC006', 'BR002', 'Esive Corp', '10 Seafield Pl', 'Foston'
+	'BC006', 'BR002', 'Esive Corp', '10 Seafield Pl', 'Glasgow'
 );
 INSERT INTO BusinessClient VALUES (
-	'BC007', 'BR003', 'Micron Systems', '27 Stone St', 'Bagworth'
+	'BC007', 'BR003', 'Micron Systems', '27 Stone St', 'Dry Fork'
 );
 INSERT INTO BusinessClient VALUES (
 	'BC008', 'BR001', 'Trivu Engineering', '25 Abbey Row', 'Kilninian'
 );
 INSERT INTO BusinessClient VALUES (
-	'BC009', 'BR002', 'Omninyx Food Supply', '12 Stoic St', 'Foston'
+	'BC009', 'BR002', 'Omninyx Food Supply', '12 Stoic St', 'Lamb'
 );
 INSERT INTO BusinessClient VALUES (
 	'BC010', 'BR003', 'Quamb Corp', '48 Asfordby Rd', 'Aldingham'
--- insert_private.sql
+++ insert_private.sql
@@ -5,19 +5,19 @@ spool /home/csusb.edu/005940658/CSE572/CSE572S19/Project/output_scripts/insert_p
 SET CONSTRAINTS ALL DEFERRED;
 
 INSERT INTO PrivateClient VALUES (
-	'CL001', 'BR002', 'Liam', 'Page', 'Male', '30-Dec-88', '76 Pier Road', 'Stanton Lacy'
+	'CL001', 'BR002', 'Liam', 'Page', 'Male', '30-Dec-88', '76 Pier Road', 'Dry Fork'
 );
 INSERT INTO PrivateClient VALUES (
 	'CL002', 'BR001', 'Andrew',  'Coates', 'Male', '26-Nov-58', '84 Holagate Rd', 'Raithby'
 );
 INSERT INTO PrivateClient VALUES (
-	'CL003', 'BR003', 'Oliver',  'Fry', 'Male', '21-Dec-90', '48 Foregate St', 'Codsall Wood'
+	'CL003', 'BR003', 'Oliver',  'Fry', 'Male', '21-Dec-90', '48 Foregate St', 'Glasgow'
 );
 INSERT INTO PrivateClient VALUES (
 	'CL004', 'BR001', 'Robert',  'Hall', 'Male', '10-Jul-91', '34 Ploughley Rd', 'Tirdeunaw'
 );
 INSERT INTO PrivateClient VALUES (
-	'CL005', 'BR001', 'Finlay',  'Nelson', 'Male', '25-Jul-91', '16 Consett Rd', 'Hill Dyke'
+	'CL005', 'BR001', 'Finlay',  'Nelson', 'Male', '25-Jul-91', '16 Consett Rd', 'Lamb'
 );
 INSERT INTO PrivateClient VALUES (
 	'CL006', 'BR002', 'Mohammad', 'Tucker', 'Male', '17-Nov-92', '33 Traill St', 'Rothiebrisbane'
@@ -26,16 +26,16 @@ INSERT INTO PrivateClient VALUES (
 	'CL007', 'BR001', 'Libby', 'Power', 'Female', '31-Jan-93', '75 Wartnaby Rd', 'Acrefair'
 );
 INSERT INTO PrivateClient VALUES (
-	'CL008', 'BR002', 'Nicholas', 'Poole', 'Male', '5-Jun-93', '8 Glenurquhart Road', 'Balgavies'
+	'CL008', 'BR002', 'Nicholas', 'Poole', 'Male', '5-Jun-93', '8 Glenurquhart Road', 'Dry Fork'
 );
 INSERT INTO PrivateClient VALUES (
-	'CL009', 'BR003', 'Jack', 'Webb', 'Male', '26-Dec-94', '73 Winchester Rd', 'Methyr Cynog'
+	'CL009', 'BR003', 'Jack', 'Webb', 'Male', '26-Dec-94', '73 Winchester Rd', 'Glasgow'
 );
 INSERT INTO PrivateClient VALUES (
 	'CL010', 'BR003', 'Louie', 'Kelly', 'Male', '12-Jun-95', '17 Ross Rd', 'Marshbrook'
 );
 INSERT INTO PrivateClient VALUES (
-	'CL011', 'BR001', 'Taylor', 'Shaw', 'Female', '4-Aug-95', '72 Guildry St', 'Gartachoil'
+	'CL011', 'BR001', 'Taylor', 'Shaw', 'Female', '4-Aug-95', '72 Guildry St', 'Lamb'
 );
 INSERT INTO PrivateClient VALUES (
 	'CL012', 'BR002', 'Mason', 'Parsons', 'Male', '20-Aug-95', '57 North Promenade', 'Dolgellau'
@@ -44,7 +44,7 @@ INSERT INTO PrivateClient VALUES (
 	'CL013', 'BR002', 'Joseph', 'Powell', 'Male', '4-Jul-96', '85 Stamford Rd', 'Marshbrook'
 );
 INSERT INTO PrivateClient VALUES (
-	'CL014', 'BR003', 'Eloise', 'Franklin', 'Female', '14-Feb-97', '21 Park Ave', 'Hill Dyke'
+	'CL014', 'BR003', 'Eloise', 'Franklin', 'Female', '14-Feb-97', '21 Park Ave', 'Lamb'
 );
 INSERT INTO PrivateClient VALUES (
 	'CL015', 'BR001', 'Zak', 'Wong', 'Male', '8-Nov-97', '98 Fordham Rd', 'Rothiebrisbane'
@@ -56,13 +56,13 @@ INSERT INTO PrivateClient VALUES (
 	'CL017', 'BR002', 'Scarlett', 'Griffiths', 'Female', '16-Jan-99', '16 Nith St', 'Balgavies'
 );
 INSERT INTO PrivateClient VALUES (
-	'CL018', 'BR001', 'Charlie', 'Glover', 'Male', '23-Jul-91', '11 Stamford Rd', 'Gartachoil'
+	'CL018', 'BR001', 'Charlie', 'Glover', 'Male', '23-Jul-91', '11 Stamford Rd', 'Glasgow'
 );
 INSERT INTO PrivateClient VALUES (
 	'CL019', 'BR003', 'Joel', 'Norton', 'Male', '10-Nov-92', '49 Bouverie Rd', 'MarshBrook'
 );
 INSERT INTO PrivateClient VALUES (
-	'CL020', 'BR001', 'Nicole', 'Rowe', 'Female', '16-Jan-01', '7 Stamford Rd', 'Acrefair'
+	'CL020', 'BR001', 'Nicole', 'Rowe', 'Female', '16-Jan-01', '7 Stamford Rd', 'Dry Fork'
 );
 
 -- Set defered constraints effective. 
